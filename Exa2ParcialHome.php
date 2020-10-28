04de5d8d1011977fad71ba8149f1e832f0b6757c
JesusOlmosLarios <69598051+JesusOlmosLarios@users.noreply.github.com>
2020-10-28 00:37:47 -0600

    Header Completo 1

--- Exa2ParcialHome.php
+++ Exa2ParcialHome.php
@@ -6,10 +6,6 @@ require_once "model.php";
 
     include("_Exa2ParcialTables.html");
 
-    include("_EliminarCliente.html");
-
-    include("_ActualizarCliente.html");
-
     include("_Exa2ParcialInfo.html");
 
     include("_Exa2ParcialFooter.html");
