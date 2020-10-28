ab7e9f6902273fd515e2d58de40dde960e091e33
JesusOlmosLarios <69598051+JesusOlmosLarios@users.noreply.github.com>
2020-10-28 17:06:55 -0600

    Falta poco

--- _Consulta3.php
+++ _Consulta3.php
@@ -5,7 +5,7 @@ require_once "model.php";
     include("_Exa2ParcialHeader.html");
 
     echo '<h3>Todos los registros de zombis con todas las actualizaciones de cada uno.</h3>';
-    echo Registros_Zombies();
+    echo Registros_Actualizados();
 
     include("_Exa2ParcialFooter.html");
 
