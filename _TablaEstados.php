22116e5d5b8f967a564ebe30a0689e6fec5f0a22
JesusOlmosLarios <69598051+JesusOlmosLarios@users.noreply.github.com>
2020-10-28 16:13:33 -0600

    Ya casi

--- _TablaEstados.php
+++ _TablaEstados.php
@@ -5,7 +5,7 @@ require_once "model.php";
     include("_Exa2ParcialHeader.html");
 
     echo '<h3>¡CONOCE LOS ESTADOS DE LOS ZOMBIES!</h3>';
-    echo tablaEstados();
+    echo tablaEstado();
 
     include("_Exa2ParcialFooter.html");
 
